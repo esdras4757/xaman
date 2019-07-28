c2b5758de4d90107f2811cb09a44f92cae29404a
esdras4757 <53359532+esdras4757@users.noreply.github.com>
2019-07-27 22:13:15 -0500

    copas

--- app/src/main/java/com/example/inicio/cliente.java
+++ app/src/main/java/com/example/inicio/cliente.java
@@ -2,7 +2,9 @@ package com.example.inicio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class cliente extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class cliente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente);
+    }
+
+    public void progreso(View view){
+
+        Intent intent= new Intent(cliente.this,Progreso.class);
+        startActivity(intent);
+
     }
 }
